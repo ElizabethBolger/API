48a8acab6a0b58f52f88643fbb26ba8f514a491d
Elizabeth Bolger <bolgere3@tcd.ie>
2019-11-08 11:35:40 +0000

    commit 2

--- GitAPI2.R
+++ GitAPI2.R
@@ -1,24 +1,27 @@
-#install.packages
+#install.packages("jsonlite")
 library(jsonlite)
+#install.packages("httpuv")
 library(httpuv)
+#install.packages("httr")
 library(httr)
 
+# Can be github, linkedin etc depending on application
 oauth_endpoints("github")
 
-myapp = oauth_app(appname = "Software_Engineering_API",
-                  key = "8d5f8e3baf1d5224e3e2",
-                  secret = "43116acd50511ba7c882f14f7e1249bbefef0b01")
+# Change based on what you 
+myapp <- oauth_app(appname = "Access_Github",
+                   key = "8d5f8e3baf1d5224e3e2",
+                   secret = "43116acd50511ba7c882f14f7e1249bbefef0b01")
 
 # Get OAuth credentials
-github_token = oauth2.0_token(oauth_endpoints("github"), myapp)
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
 
 # Use API
-gtoken = config(token = github_token)
-UserFollowingData = GET("https://api.github.com/users/jtleek/following", gtoken)
-UserFollowingDataContent = content(UserFollowingData)
-UserFollowingDataFrame = jsonlite::fromJSON(jsonlite::toJSON(UserFollowingDataContent))
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
 
-followersLogins = c(UserFollowingDataFrame$login)
+# Take action on http error
+stop_for_status(req)
 
 # Extract content from a request
 json1 = content(req)
